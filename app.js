450cf874d28b78a07c7371a165f87ea7e21f381f
Lukas Bühler <lukasmc6@gmail.com>
2017-11-27 00:10:59 +0100

    got camera footage from drone

--- app.js
+++ app.js
@@ -9,103 +9,113 @@ var drone = arDrone.createClient();
 // Create the web server
 app.use(express.static(__dirname + '/page')); // Uses the HTML files from the page directory
 
-app.get('/', function (req, res) 
-{
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'page', 'index.html')); // displays the main page
 });
 
 
 // Drone control
-app.get(/\w+$/, function (req, res) 
-{
-    var func = req.originalUrl.substr(1);
-    if(typeof dadroneFunctions[func] !== "undefined")
-    {
+app.get(/\/drone\/\w+$/, function (req, res) {
+    var func = req.originalUrl.substr(7);
+    if (typeof dadroneFunctions[func] !== "undefined") {
         // We know this function
         var funcText = dadroneFunctions[func](); // Calls the requested function
         res.send(funcText);
     }
-    else
-    {
+    else {
         // Unknown function requested
-        res.send("Unknown function "+func);
+        res.send("Unknown function " + func);
     }
-    
+
 });
 
-// The funcitons
-var dadroneFunctions = 
-{
-    // Modes
-    "normal": function() {
-        // 
-    
-        return "Switching to normal mode.";
-    },
-    "acro": function() {
-        // 
-    
-        return "Switching to acro mode.";
-    },
-    "watchdog": function() {
-        // 
-    
-        return "Switching to watchdog mode.";
-    },
-    "scout": function() {
-        // 
-    
-        return "Switching to scout mode.";
-    },
-    "fighter": function() {
-        // 
-    
-        return "Switching to fighter mode.";
-    },
+// images
+var pngStream = drone.getPngStream();
+var pngBuffer = null;
 
-    // All time
-    "stop": function() {
-        // 
-    
-        return "Holding still!";
-    },
-    
-    // Normal
-    "takeoff": function() 
-    {
-        // Lets the drone take off and keeps it suspended 1m above the ground.
-    
-        drone.takeoff();
-        return "Taking off!";
-    },
-    "land": function() 
-    {
-        // Slowly lets the drone go down and land on the floor. Also turns the rotors off when landed.
+pngStream
+    .on('error', console.log)
+    .on('data', function (buffer) {
+        pngBuffer = buffer;
+    });
 
-        drone.land();
-        return "Landing without checking";
-    },
-    "safeland": function() 
+app.get('/cameras/front/png', function (req, res) {
+    while(pngBuffer === null)
     {
-        // Check if it is safe to land on the floor and only land when it is. If it isn’t safe to land go to a safe landing location.
-
-        
-        return "Landing safely";
-    },
-    "shutdown": function() 
-    {
-        // 
-    
-        return "Stopping all motor functions.";
-    },
-    
-    // Acro
-    "flip": function() 
-    {
-        // 
-    
-        console.log("Perfroming flip!");
+        // waiting
     }
-};
+    res.send(pngBuffer); // displays the main page
+});
+
+
+// The funcitons
+var dadroneFunctions =
+    {
+        // Modes
+        "normal": function () {
+            // 
+
+            return "Switching to normal mode.";
+        },
+        "acro": function () {
+            // 
+
+            return "Switching to acro mode.";
+        },
+        "watchdog": function () {
+            // 
+
+            return "Switching to watchdog mode.";
+        },
+        "scout": function () {
+            // 
+
+            return "Switching to scout mode.";
+        },
+        "fighter": function () {
+            // 
+
+            return "Switching to fighter mode.";
+        },
+
+        // All time
+        "stop": function () {
+            // 
+
+            return "Holding still!";
+        },
+
+        // Normal
+        "takeoff": function () {
+            // Lets the drone take off and keeps it suspended 1m above the ground.
+
+            drone.takeoff();
+            return "Taking off!";
+        },
+        "land": function () {
+            // Slowly lets the drone go down and land on the floor. Also turns the rotors off when landed.
+
+            drone.land();
+            return "Landing without checking";
+        },
+        "safeland": function () {
+            // Check if it is safe to land on the floor and only land when it is. If it isn’t safe to land go to a safe landing location.
+
+
+            return "Landing safely";
+        },
+        "shutdown": function () {
+            // 
+
+            return "Stopping all motor functions.";
+        },
+
+        // Acro
+        "flip": function () {
+            // 
+
+            console.log("Perfroming flip!");
+        }
+    };
 
 app.listen(3000, () => console.log('App is online on http://localhost:3000'));
--- page/controller.js
+++ page/controller.js
@@ -1,22 +1,49 @@
-function call(pathname) 
+
+
+$(document).ready(function () 
 {
+    getCameraImage();
+});
+
+function call(pathname) {
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
-            if(xhr.status === 200)
-            {
+            if (xhr.status === 200) {
                 console.log(xhr.response); //Outputs a DOMString by default
             }
-            else
-            {
+            else {
                 // Got error
                 console.log(xhr.statusText);
             }
-          
-        }
-      }
-    xhr.open('GET', pathname, true);
-    xhr.send();
 
-    console.log("calling /"+pathname);
+        }
+    }
+    xhr.open('GET', "drone/"+pathname, true);
+    xhr.send();
+}
+
+function getCameraImage()
+{
+    // Get PNG stream and stuff
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var blob = xhr.response;
+                var image = "data:image/jpeg;base64,"+btoa(String.fromCharCode.apply(null, new Uint8Array(blob)));
+
+                $("#front-camera-image").attr("src", image);
+                getCameraImage();
+            }
+            else {
+                // Got error
+                console.log(xhr.statusText);
+            }
+
+        }
+    }
+    xhr.open('GET', "/cameras/front/png", true);
+    xhr.responseType = "arraybuffer";
+    xhr.send();
 }
